ac9a19a9346bef2ceb86895a100753d86234501e
Olena Hapon <olenagapon90gmail.com>
2025-05-08 01:42:51 +0300

    Changes

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,8 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// // https://vite.dev/config/
+// export default defineConfig({
+//   base: '/Chat_app/',
+//   plugins: [react()],
+// })
+// vite.config.ts
+
 export default defineConfig({
   base: '/Chat_app/',
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      '/api': 'http://localhost:3000', // або на продакшн сервер, наприклад на Render
+    },
+  },
+});
