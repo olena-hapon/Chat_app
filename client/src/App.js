c56d65bb60d0048c9f1169074b46203d4f2d8145
Olena Hapon <olenagapon90gmail.com>
2025-05-09 08:22:26 +0300

    Fix: remove compiled .js files from src

--- client/src/App.js
+++
@@ -1,6 +0,0 @@
-import { Fragment as _Fragment, jsx as _jsx } from "react/jsx-runtime";
-import './App.css';
-function App() {
-    return (_jsx(_Fragment, {}));
-}
-export default App;
--- client/src/routes/signinPage/SigninPage.js
+++
@@ -1,7 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-import { SignIn } from "@clerk/clerk-react";
-import './signinPage.css';
-const SigninPage = () => {
-    return (_jsx("div", { className: "signInPage", children: _jsx(SignIn, { path: "/sign-in", signUpUrl: "/sign-up", forceRedirectUrl: "/dashboard" }) }));
-};
-export default SigninPage;
